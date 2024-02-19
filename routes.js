0c87493cb3ae48cfee4b50f56fc62e0c3490bcfa
Ренета Бонева <reneta.boneva@abv.bg>
2024-02-19 17:39:30 +0200

    Add router

---
+++ routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+// // on authentication
+// router.post('/register')
+// router.post('/login')
+// router.get('/logout')
+
+// // on my profile page
+// router.get('/user/:_userID')
+// // on level up
+// router.patch('/user/:_userID')
+
+module.exports = router;
