9efb020a000a9b56752c84df66d4690caf3bd0bc
Ренета Бонева <reneta.boneva@abv.bg>
2024-02-21 18:16:58 +0200

    Add isOwner middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -8,10 +8,30 @@ exports.isAuth = async (req, res, next) => {
         try {
             await jwt.verify(accessToken, SECRET);
         } catch (err) {
-            return res.status(404).json({ "Error": err.message });
+            return res.status(401).json({ "Error": err.message });
         }
     } else {
-        return res.status(404).json({ "Error": "Unauthorized!" });
+        return res.status(401).json({ "Error": "Unauthorized!" });
+    }
+
+    next();
+}
+
+exports.isOwner = async (req, res, next) => {
+    const { _userID } = req.params;
+    const accessToken = req.headers["x-authorization"];
+
+    if (accessToken) {
+        try {
+            const verifiedToken = await jwt.verify(accessToken, SECRET);
+            if (_userID != verifiedToken._userID) {
+                throw new Error("Unauthorized!");
+            };
+        } catch (err) {
+            return res.status(401).json({ "Error": err.message });
+        }
+    } else {
+        return res.status(401).json({ "Error": "Unauthorized!" });
     }
 
     next();
