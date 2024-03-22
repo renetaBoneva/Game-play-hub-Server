f18075ce46c0ef0f7850c7833f3734d2f0cd1dcb
Ренета Бонева <reneta.boneva@abv.bg>
2024-03-22 16:26:20 +0200

    Change API Key

--- constants.js
+++ constants.js
@@ -6,4 +6,4 @@ const dbCredentials = {
 exports.dbConnectionString = `mongodb+srv://${dbCredentials.username}:${dbCredentials.password}@gameplayhub.jkjdleh.mongodb.net/?retryWrites=true&w=majority`;
 exports.port = 3333;
 exports.SECRET = '2a2e13724ac9764654700bee56f4185c181ebe61';
-exports.OPEN_AI_KEY = '4fe7805fe9504a9e04b7b714baaf77e24356ad7474be9448db0c5207849e8000';
+exports.OPEN_AI_KEY = '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';
