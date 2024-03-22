bb60b27b71708cc4f43ad853b04ccf68f9b39c22
Ренета Бонева <reneta.boneva@abv.bg>
2024-03-22 14:32:43 +0200

    Make route for ai response for tic tac toe game

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const authController = require('./controllers/authController');
+const ticTacToeController = require('./controllers/ticTacToeController');
 const { isAuth, isOwner } = require('./middlewares/authMiddleware');
 
 router.get('/', (req, res) => {
@@ -17,5 +18,7 @@ router.get('/users/:_userID', isOwner, authController.getProfile);
 router.delete('/users/:_userID', isOwner, authController.deleteProfile);
 // // on level up
 // router.patch('/users/:_userID');
+
+router.get('games/ticTacToe/AIresponse', ticTacToeController.AIresponse);
 
 module.exports = router;
