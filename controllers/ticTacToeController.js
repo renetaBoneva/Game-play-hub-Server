b81ddbb5ae08368d25995c0b87acd4392fb0b9a4
Ренета Бонева <reneta.boneva@abv.bg>
2024-03-22 14:32:08 +0200

    Create tic tac toe controller and AIresponse function

---
+++ controllers/ticTacToeController.js
@@ -0,0 +1,37 @@
+const { OPEN_AI_KEY } = require("../constants");
+const { OpenAI } = require('openai');
+
+exports.AIresponse = async (req, res) => {
+    const openai = new OpenAI({ apiKey: OPEN_AI_KEY });
+    const aiModel = 'gpt-4-turbo-preview';
+    const boardJSON = req.query.boardl
+
+    if (boardJSON && boardJSON.length === 3) {
+        const prompt = [];
+        prompt.push('You are an expert tic tac toe player.');
+        prompt.push('You play as O. Focus on winning. Play extremely well.');
+        prompt.push('For the json content i provide as input, please give me json output in this format:');
+        prompt.push('{board:[[],[],[]]}');
+
+        const messages = [
+            {
+                role: 'system',
+                content: prompt.join(' ')
+            },
+            {
+                role: 'user',
+                content: boardJSON
+            }
+        ];
+
+        const completion = await openai.chat.completions.create({
+            model: aiModel,
+            messages,
+            response_format: { 'type': 'json_object' }
+        })
+
+        const aiResponse = completion.choices[0].message.content;
+        res.statues(200).json({ aiResponse });
+    }
+    res.statues(400).json({ "Error": "Incorrect input data!" });
+}
